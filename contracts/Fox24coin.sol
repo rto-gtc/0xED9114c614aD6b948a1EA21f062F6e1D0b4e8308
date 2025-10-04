10a1fd52f31d01543e47e519bf6fc926ec994287
rto-gtc <rto.gtc@gmail.com>
2025-10-04 19:32:23 +0200

    Initial source code push (forced)

---
+++ contracts/Fox24coin.sol
@@ -0,0 +1,34 @@
+﻿// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.4.0
+pragma solidity ^0.8.27;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
+
+contract Fox24coin is ERC20, ERC20Burnable, ERC20Permit, ERC20Votes {
+    constructor(address recipient)
+        ERC20("Fox24coin", "Fox24")
+        ERC20Permit("Fox24coin")
+    {
+        _mint(recipient, 100000 * 10 ** decimals());
+    }
+
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._update(from, to, value);
+    }
+
+    function nonces(address owner)
+        public
+        view
+        override(ERC20Permit, Nonces)
+        returns (uint256)
+    {
+        return super.nonces(owner);
+    }
+}
